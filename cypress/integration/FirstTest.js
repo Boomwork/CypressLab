2a385fe236398e979bdd9a7a470d297d02dfae78
voornaam achternaam <robert.verboom@atos.net>
2020-12-02 16:21:47 +0100

    1 cypress project

--- cypress/integration/FirstTest.js
+++ cypress/integration/FirstTest.js
@@ -5,9 +5,6 @@ describe('My First Test Suite', () => {
         cy.get('#search_query_top').type('Women');
         cy.get('.button-search').click();
         cy.get('#searchbox').submit();
-        cy.url().should('contains', 'http://automationpractice.com/index.php');
-        cy.get('.sfHover > .sf-with-ul').click();
-        cy.url().should('contains', 'http://automationpractice.com/index.php');
     });
 })
 
